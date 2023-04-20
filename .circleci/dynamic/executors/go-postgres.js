7dbbfb52d518ec067fdc17163390b9d0dda20d9a
nokeefe <nick.okeefe@cirlceci.com>
2023-04-20 10:53:01 -0700

    fix: removed dupe cci const

--- .circleci/dynamic/executors/go-postgres.js
+++ .circleci/dynamic/executors/go-postgres.js
@@ -5,7 +5,6 @@ const go = new CircleCI.executors.DockerExecutor(
 );
 module.exports = goPostgres;
 
-const CircleCI = require("@circleci/circleci-config-sdk");
 const postgres = new CircleCI.executors.DockerExecutor(
     "cimg/postgres:15.1",
     "medium"
