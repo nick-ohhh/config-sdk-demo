b8550b9c6dba0724a87831ee9d6395e0a6cf5f92
nokeefe <nick.okeefe@cirlceci.com>
2023-04-20 10:53:49 -0700

    fix: exports

--- .circleci/dynamic/executors/go-postgres.js
+++ .circleci/dynamic/executors/go-postgres.js
@@ -3,9 +3,10 @@ const go = new CircleCI.executors.DockerExecutor(
     "cimg/go:1.19.5",
     "medium"
 );
-module.exports = goPostgres;
+module.exports = go;
 
 const postgres = new CircleCI.executors.DockerExecutor(
     "cimg/postgres:15.1",
     "medium"
 );
+module.exports = postgres;
