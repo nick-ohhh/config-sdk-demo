80501c73f2ca4166851fd3a7b9fa9ed98783eb9a
nokeefe <nick.okeefe@cirlceci.com>
2023-04-20 11:59:08 -0700

    test: go and postgres image diff

--- .circleci/dynamic/jobs/bazelBuild.js
+++ .circleci/dynamic/jobs/bazelBuild.js
@@ -4,11 +4,11 @@ const postgres = require("../executors/go-postgres");
 
 const bazelBuildgo = new CircleCI.Job("bazelBuildgo", go);
 bazelBuildgo.addStep(new CircleCI.commands.Checkout());
-bazelBuildgo.addStep(new CircleCI.commands.Run({ command: "echo test for go and postgres images" }));
+bazelBuildgo.addStep(new CircleCI.commands.Run({ command: "echo test for go image" }));
 
 const bazelBuildpostgres = new CircleCI.Job("bazelBuildpostgres", postgres);
 bazelBuildpostgres.addStep(new CircleCI.commands.Checkout());
-bazelBuildpostgres.addStep(new CircleCI.commands.Run({ command: "echo test for go and postgres images" }));
+bazelBuildpostgres.addStep(new CircleCI.commands.Run({ command: "echo test for postgres image" }));
 
 module.exports = bazelBuildgo;
 module.exports = bazelBuildpostgres;
