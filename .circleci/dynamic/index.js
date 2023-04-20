f2519d4fb6d3a1fc91f39420bca119c66284715c
nokeefe <nick.okeefe@cirlceci.com>
2023-04-20 10:25:25 -0700

    fix: job name updated

--- .circleci/dynamic/index.js
+++ .circleci/dynamic/index.js
@@ -2,7 +2,8 @@ const CircleCI = require("@circleci/circleci-config-sdk");
 
 // Import Config Components
 const jobA = require("./jobs/jobA");
-const jobB = require("./jobs/jobB");
+const buildGoImage = require("./jobs/bazelBuild");
+const buildGoImage = require("./jobs/bazelBuild");
 
 // Initiate a new Config
 const myConfig = new CircleCI.Config()
@@ -10,17 +11,17 @@ const myConfig = new CircleCI.Config()
 // Add elements to the config
 myConfig
     .addJob(jobA)
-    .addJob(jobB)
+    .addJob(buildGoImage)
 
 // Instantiate new Workflow and add jobA
 const dynamicWorkflow = new CircleCI.Workflow("dynamic-workflow");
 dynamicWorkflow.addJob(jobA);
 
-// Conditionally add jobB
+// Conditionally add buildGoImage
 // This condition could be based on anything from the response of an API call
 // to the contents of a file
 if (1 == 1) {
-    dynamicWorkflow.addJob(jobB);
+    dynamicWorkflow.addJob(buildGoImage);
 }
 
 // Add the dynamic workflow to the config
